79af3746cc9f217cfaf3eb3d74c42eca4a8e717e
James Huang <hng.jms@gmail.com>
2018-10-04 20:08:12 -0700

    Don't overide acodec if parameter is specified
    File conversion attempts to maintain format.
    Don't do this if we explicitly specify an output format.
    
    CR: jeremy

--- pydub/audio_segment.py
+++ pydub/audio_segment.py
@@ -662,24 +662,27 @@ class AudioSegment(object):
             stdin_parameter = subprocess.PIPE
             stdin_data = file.read()
 
-        info = mediainfo_json(orig_file)
-        if info:
-            audio_streams = [x for x in info['streams']
-                             if x['codec_type'] == 'audio']
-            # This is a workaround for some ffprobe versions that always say
-            # that mp3/mp4/aac/webm/ogg files contain fltp samples
-            if (audio_streams[0].get('sample_fmt') == 'fltp' and
-                    (is_format("mp3") or is_format("mp4") or is_format("aac") or
-                     is_format("webm") or is_format("ogg"))):
-                bits_per_sample = 16
-            else:
-                bits_per_sample = audio_streams[0]['bits_per_sample']
-            if bits_per_sample == 8:
-                acodec = 'pcm_s8'
-            else:
-                acodec = 'pcm_s%dle' % bits_per_sample
+        if not "-acodec" in parameters:
+            # specify codec on output side
+            info = mediainfo_json(orig_file)
+            if info:
+                audio_streams = [x for x in info['streams']
+                                 if x['codec_type'] == 'audio']
+                # This is a workaround for some ffprobe versions that always say
+                # that mp3/mp4/aac/webm/ogg files contain fltp samples
+                if (audio_streams[0].get('sample_fmt') == 'fltp' and
+                        (is_format("mp3") or is_format("mp4") or is_format("aac") or
+                         is_format("webm") or is_format("ogg"))):
+                    bits_per_sample = 16
+                else:
+                    bits_per_sample = audio_streams[0]['bits_per_sample']
+                if bits_per_sample == 8:
+                    acodec = 'pcm_s8'
+                else:
+                    acodec = 'pcm_s%dle' % bits_per_sample
 
-            conversion_command += ["-acodec", acodec]
+                conversion_command += ["-acodec", acodec]
+
         if parameters is not None:
             # extend arguments with arbitrary set
             conversion_command.extend(parameters)
